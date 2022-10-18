aff477fb2f185fd8e85b498edb6ba435c52f9347
ianmajkut <ianedwinmajkut@gmai.com>
2022-10-18 14:37:51 -0300

    ADD implementacion de modal dinámico para /misTrivias para editar o comenzar una trivia. TODO implementar funcionalidad al modal de editar con forms

---
+++ src/app/shared/components/modal-mis-trivias/modal-mis-trivias.component.ts
@@ -0,0 +1,52 @@
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TipoDePregunta, Trivia } from 'app/interfaces/Trivias.interface';
+import { PartidasService } from 'app/services/partidas.service';
+import { SocketService } from 'app/socket/socket.service';
+
+@Component({
+  selector: 'app-modal-mis-trivias',
+  templateUrl: './modal-mis-trivias.component.html',
+  styleUrls: ['./modal-mis-trivias.component.css']
+})
+export class ModalMisTriviasComponent implements OnInit {
+  constructor(
+    private fb: FormBuilder,
+    private _partidasService: PartidasService,
+    private _socketsService: SocketService,
+    private router: Router
+  ) {}
+
+
+
+  @Input() trivia!: Trivia;
+  @Input() modalPorAbrir!: string;
+  @ViewChild('btnModalCerrar') btnModalCerrar!: ElementRef;
+
+  tiposDePreguntas: string[] = ['multiple_choice', 'single_choice', 'votacion'];
+  esCorrecta: boolean[] = [true, false];
+
+  crearForm: FormGroup = this.fb.group({
+    segundosEntrePreguntas: ['', [Validators.required, Validators.min(5)]]
+  });
+
+
+  ngOnInit(): void {}
+
+  crearPartida() {
+    // console.log(this.crearForm.value['segundosEntrePreguntas']);
+    this._partidasService
+      .crearPartida(
+        this.trivia.id,
+        this.crearForm.value['segundosEntrePreguntas']
+      )
+      .subscribe((res: any) => {
+        const partida = res.body;
+        this._socketsService.unirse(partida._usuarioHost.id, partida.id);
+        //Clickeamos el btn con data-bs-dismiss="modal" para cerrar el modal
+        this.btnModalCerrar.nativeElement.click();
+        this.router.navigateByUrl(`/main/lobby/${partida.id}`);
+      });
+  }
+}
