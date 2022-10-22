07cff61b03b160c6b575827a9406cf2c5b6c90be
ianmajkut <ianedwinmajkut@gmai.com>
2022-10-22 12:39:58 -0300

    FIX modal unirse partida

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PartidasService } from 'app/services/partidas.service';
+import { ToastService } from 'app/services/toast.service';
 import { UserService } from 'app/services/user.service';
 import { SocketService } from 'app/socket/socket.service';
 
@@ -16,6 +17,7 @@ export class ModalComponent implements OnInit {
     private _partidasSevice: PartidasService,
     private _socketsService: SocketService,
     private _userService: UserService,
+    private toastService: ToastService,
     private router: Router
   ) {}
   @Input('modalPorAbrir') modalPorAbrir!: string;
@@ -30,26 +32,41 @@ export class ModalComponent implements OnInit {
   ingresarPartida() {
     this._userService.getIdUser().subscribe((res: any) => {
       const idUser = res.body;
-      this._partidasSevice.getPartidas().subscribe((res: any) => {
-        //Si no tiene partidas el usuario
-        if (res.body.length == 0) {
-          this._socketsService.unirse(
-            idUser,
-            this.ingresarPartidaForm.value['codigo']
-          );
-          this.btnModalCerrar.nativeElement.click();
-          this.router.navigateByUrl(
-            `/main/lobby/${this.ingresarPartidaForm.value['codigo']}`
-          );
-        }
-        //Si la partida que encontro 
-        if (res.body[0].id == this.ingresarPartidaForm.value['codigo']) {
-          this.btnModalCerrar.nativeElement.click();
-          this.router.navigateByUrl(
-            `/main/lobby/${this.ingresarPartidaForm.value['codigo']}`
-          );
-        }
-      });
+      return this._partidasSevice
+        .obtenerPartida(this.ingresarPartidaForm.value['codigo'])
+        .then((res: any) => {
+          //Si coincide el id de la partida con alguna partida existente
+          if (res.id == this.ingresarPartidaForm.value['codigo']) {
+            this._partidasSevice.getPartidas().subscribe((res: any) => {
+              //Si no tiene ninguna partida el usuario
+              if (res.body.length == 0) {
+                this._socketsService.unirse(
+                  idUser,
+                  this.ingresarPartidaForm.value['codigo']
+                );
+                this.toastService.showSuccess(
+                  'Te has unido a la partida correctamente',
+                  'Felicidades'
+                );
+                this.btnModalCerrar.nativeElement.click();
+                this.router.navigateByUrl(
+                  `/main/lobby/${this.ingresarPartidaForm.value['codigo']}`
+                );
+              }
+              //Si ya esta unido en alguna
+              this.btnModalCerrar.nativeElement.click();
+              this.router.navigateByUrl(
+                `/main/lobby/${this.ingresarPartidaForm.value['codigo']}`
+              );
+            });
+            return
+          } return this.toastService.showError('No se encontró ninguna partida con ese id', 'Error');
+        })
+        .catch((error: any) => {
+          // console.log(error.error.body);
+          //Si tenemos algun error del server nos manda al main/home
+          this.toastService.showError(error.error.body, 'Error');
+        });
     });
   }
 }
