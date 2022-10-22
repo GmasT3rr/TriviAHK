2e7ef2590002dc26a1e9852c1985ed8d71e20881
ianmajkut <ianedwinmajkut@gmai.com>
2022-10-22 15:17:09 -0300

    ADD creacion de directiva estructurada para mostrar contenido exclusivo al host y si deseamos especificar que contenido es para el jugador

--- src/app/pages/lobby/main/lobby.component.ts
+++ src/app/pages/lobby/main/lobby.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { faCoffee, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-lobby',
   templateUrl: './lobby.component.html',
-  styleUrls: ['./lobby.component.scss'],
+  styleUrls: ['./lobby.component.scss']
 })
 export class LobbyComponent implements OnInit {
+  constructor(private router: Router) {}
   faUsers = faUsers;
-  constructor() {}
+  idPartida: number = 0;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const urlLobby = this.router.url.split('/');
+    this.idPartida = Number(urlLobby[urlLobby.length - 1]);
+    // console.log(idPartida);
+  }
 }
--- src/app/shared/directives/es-host.directive.ts
+++ src/app/shared/directives/es-host.directive.ts
@@ -1,19 +1,53 @@
 import {
   Directive,
+  Input,
   OnInit,
   TemplateRef,
   ViewContainerRef
 } from '@angular/core';
+import { PartidasService } from 'app/services/partidas.service';
 
 @Directive({
-  selector: '[appEsHost]'
+  selector: '[esHost]'
 })
 export class EsHostDirective implements OnInit {
   constructor(
     private templateRef: TemplateRef<any>,
-    private viewContainer: ViewContainerRef
+    private viewContainer: ViewContainerRef,
+    private _partidaService: PartidasService
   ) {}
+
+  // @Input('esHost') idPartida!: number;
+  @Input('esHost') inputRecibido!: {
+    idPartida: number;
+    renderContentUser: boolean;
+  };
+
+  @Input('esHostSino') esUsuario!: TemplateRef<any>;
+
   ngOnInit(): void {
-    this.viewContainer.createEmbeddedView(this.templateRef);
+    // console.log(this.idPartida);
+    //Agarro endpoint para obtener las partidas creadas del usuario
+    this._partidaService.getPartidas().subscribe((res: any) => {
+      //Si me devuelve una partida y el id coincide con el id de la partida significa que es el creador
+      if (
+        res.body.length > 0 &&
+        res.body[0].id == this.inputRecibido.idPartida
+      ) {
+        return this.viewContainer.createEmbeddedView(this.templateRef);
+        // return console.log('sos el creador');
+      }
+      /*
+      Esta validación es creada con el fin de no tener que
+      si o si tener el ng-template de sino esUsuario
+      Si queremos tener ng-template le mandamos renderContentUser
+      true
+      */
+      if (this.inputRecibido.renderContentUser == true) {
+        return this.viewContainer.createEmbeddedView(this.esUsuario);
+      }
+      return;
+      // return console.log('sos el player');
+    });
   }
 }
